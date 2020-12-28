42afffc5b0bd0853d991aa55a795324fe16719c8
alexpsimone <alex.p.simone@gmail.com>
2020-12-28 17:25:14 +0000

    added initial code

--- easy/unique_morse_code_words.py
+++ easy/unique_morse_code_words.py
@@ -6,13 +6,27 @@
 def uniqueMorseRepresentations(words: List[str]) -> int:
 
     morse_code_alphabet = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
+    english_alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
+    if words == []:
+        return 0
+    
     # Generate a dictionary of letters to morse code equiv.
+    english_to_morse = {}
+    for idx, letter in enumerate(english_alphabet):
+        english_to_morse[letter] = morse_code_alphabet[idx]
     # Initialize an empty set that I'll use to store unique words.
+    unique_words = set()
     # Iterate through words list.
+    for word in words:
     # Initialize an empty string that I'll use to build translated words one by one.
-    # At each word in words, iterate through each letter.
-    # For each letter, concatenate the placeholder string with the next translated letter.
-    # At the end of each word, add the string to the set.
+        translated_word = ''
+        # At each word in words, iterate through each letter.
+        for letter in word:
+        # For each letter, concatenate the placeholder string with the next translated letter.
+            translated_word += english_to_morse[letter]
+        # At the end of each word, add the string to the set.
+        unique_words.add(translated_word)
     # Finally, return the length of the set.
+    return length(unique_words)
 
