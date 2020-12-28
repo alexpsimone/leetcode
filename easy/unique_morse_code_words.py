3f0924c77d9d12684f7a8e5a1f2f161d0d15c4e2
alexpsimone <alex.p.simone@gmail.com>
2020-12-28 17:15:00 +0000

    noted unanswered questions from prompt

--- easy/unique_morse_code_words.py
+++ easy/unique_morse_code_words.py
@@ -1,3 +1,9 @@
+# Return the number of different transformations among all the words, given a list of words.
+# If empty list, will there be 1 or 0 transformations?
+# If there's a letter/character not in the alphabet, should I handle it a certain way?
+# Are the words sorted in a certain way (alphabetically, for instance)?
+# 
+
 def uniqueMorseRepresentations(words: List[str]) -> int:
 
     morse_code_alphabet = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
