145b3796a141c3fd1738565ef65a4b131c4ef89d
alexpsimone <alex.p.simone@gmail.com>
2020-12-28 21:35:17 +0000

    noted questions not answered in problem description

--- easy/maximum_subarray.py
+++ easy/maximum_subarray.py
@@ -4,4 +4,10 @@
 # Follow up: If you have figured out the O(n) solution, try coding another solution using 
 # the divide and conquer approach, which is more subtle.
 
+# What to return for empty array?
+# Is the list ordered?
+# Are all values positive/negative?
+# Do we "wrap around" from 0th to -1st index ever? I'm guessing not?
+# Range of int values: -2^31 <= nums[i] <= 2^31 - 1
+
 def maxSubArray(nums: List[int]) -> int:
