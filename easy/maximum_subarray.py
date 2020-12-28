6ad97d77eb6e9b8bae8f15ac321b526e8394ab8a
alexpsimone <alex.p.simone@gmail.com>
2020-12-28 21:45:32 +0000

    pseudocode for various solutions

--- easy/maximum_subarray.py
+++ easy/maximum_subarray.py
@@ -11,3 +11,27 @@
 # Range of int values: -2^31 <= nums[i] <= 2^31 - 1
 
 def maxSubArray(nums: List[int]) -> int:
+
+    # If the list is empty, return 0?
+    # If the list contains one value only, return that value.
+
+    # Initialize a sum with value 0.
+    # Look at sliding windows of various lengths across the list?
+    # Start with a window of length 1, go until window of length n.
+    # Slide across nums and slice the list accordingly, calculating sums across each window.
+    # If the value of the sum is greater than sum, then replace sum.
+    # Return the value of sum.
+    # I think this is more like an O(n) runtime, but there's a better way than this!
+
+    # Initialize a sum with value 0.
+    # Start at first item in the list.
+    # Find sum of the item plus the next item in the list. Then add the one after that...
+    # If a value higher than sum is found, then replace it with the new sum; otherwise keep going until the end of the list.
+    # Now go to the 2nd item in the list.
+    # Find the sum of that item plus the next item. Then add the one after that...
+    # Until you reach the end of the list.
+
+    # Return the value of sum.
+    # Slow runtime here.
+
+
