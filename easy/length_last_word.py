eaebaebf4ea4a6ce5a7937311c6a045899b0976e
alexpsimone <alex.p.simone@gmail.com>
2020-12-29 21:20:56 +0000

    solution using rstrip and list slicing

--- easy/length_last_word.py
+++ easy/length_last_word.py
@@ -1,1 +1,16 @@
+# What about trailing spaces at the end of the list, like "Hello world   "
+
 def lengthOfLastWord(s: str) -> int:
+    
+    words_list = (s.rstrip()).split(' ')
+
+    if words_list == '':
+        return 0
+    else:
+        return len(words_list[-1])
+
+# print(clean_word)
+# print(words_list)
+print(lengthOfLastWord('Hello World'))
+print(lengthOfLastWord('Hello World  '))
+print(lengthOfLastWord('    '))
