5a12057ed887308f4a1591fab55e7ba8cb6b9aee
alexpsimone <alex.p.simone@gmail.com>
2020-12-29 21:28:21 +0000

    added notes with clarifying Qs and possible solution ideas

--- easy/1295_nums_with_even_num_digits.py
+++ easy/1295_nums_with_even_num_digits.py
@@ -1,1 +1,9 @@
+# can stringify the int and find the length of the string
+
+# can divide by 10, 1000, 100000 and check mod/remainder;
+    # if mod is less than the divisor then it has the same # digits as the divisor
+
+# is list ordered?
+    # if so, and first/last digits have same num digits, then we know how many digits the rest have
+     
 def findNumbers(nums: List[int]) -> int:
