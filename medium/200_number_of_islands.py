80b26ca779d9763e9f990eb2f5cea20ca3eacf0d
alexpsimone <alex.p.simone@gmail.com>
2021-01-08 20:27:45 +0000

    initial solution

---
+++ medium/200_number_of_islands.py
@@ -0,0 +1,51 @@
+def numIslands(grid) -> int:
+
+  num_rows = len(grid)
+  num_cols = len(grid[0])
+  num_islands = 0
+  
+  # put all island vertices in the grid in a list of vertices to see  
+  unseen_land_vertices = set((x,y) for x in range(num_rows) for y in range(num_cols) if grid[x][y] == '1')
+  
+  # look until you've visited all land vertices
+  while unseen_land_vertices:
+
+    # take an item from the unseen land vertices and use it as a starting point for the BFS
+    start_point = unseen_land_vertices.pop()
+    seen_vertices = set([start_point])
+    to_see = [start_point]
+
+    while to_see:
+
+      # do a BFS from the vertex, looking at any points around it
+      x, y = to_see.pop(0)
+
+      for dx, dy in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
+
+        new_x, new_y = x + dx, y + dy
+
+        # only look at nodes in the grid that are "land", and ones which haven't been looked at before
+        if 0 <= new_x < num_rows and 0 <= new_y < num_cols and (new_x, new_y) not in seen_vertices and grid[new_x][new_y] == '1':
+          
+          to_see.append((new_x, new_y))
+          seen_vertices.add((new_x, new_y))
+          unseen_land_vertices.remove((new_x, new_y))
+    
+    # when you run out of vertices to see, you've reached the end of the connected component/island
+    num_islands += 1
+
+  return num_islands
+
+# grid = [
+#   ["1","1","1","1","0"],
+#   ["1","1","0","1","0"],
+#   ["1","1","0","0","0"],
+#   ["0","0","0","0","0"]
+# ]
+
+# grid2 = [
+#   ["1","1","0","0","0"],
+#   ["1","1","0","0","0"],
+#   ["0","0","1","0","0"],
+#   ["0","0","0","1","1"]
+# ]
