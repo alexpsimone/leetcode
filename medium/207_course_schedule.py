0adda5dcc2040cdc66a74382d24b6f61142666b9
alexpsimone <alex.p.simone@gmail.com>
2021-01-26 00:51:28 +0000

    added test cases, goals

--- medium/207_course_schedule.py
+++ medium/207_course_schedule.py
@@ -1,5 +1,20 @@
 def canFinish(numCourses, prerequisites) -> bool:
 
+    # 3, [[0,1][1,2]] --> True
+    # 4, [[0,1][2,3]] --> True
+    # 2, [[0,1][1,0]] --> False
+    # 2, [[4,2]] --> True
+    # 1, [[0,1][1,0]] --> False
+    # 2, [[0,1][0,2][1,0]] --> False
+
+    # if you find a cycle, then any node with a value in that cycle is unreachable
+    # goal: to count the num edges NOT in a cycle
+    # then compare that num to numCourses, return True/False
+
+    # create an adjacency dict: {prereq: course }
+
+
+
     pass
 
 if __name__ == '__main__':
